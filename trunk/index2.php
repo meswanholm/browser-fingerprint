648fdb016a5ee3fd5603a4e8c26dcda2412367b9
tfriepes@gmail.com <tfriepes@gmail.com@34251659-b5cb-f043-1aff-89ad90e83b3e>
2012-05-07 11:45:07 +0000

    Anmeldung PHP + Javascript

--- trunk/index2.php
+++ trunk/index2.php
@@ -1,3 +1,34 @@
 <?php
-echo phpinfo();
+ 
+require 'library/src/facebook.php';
+ 
+$facebook = new Facebook(array(
+  'appId'  => '294040014004290', // Anwendungs ID
+  'secret' => '942bf9f89444df74c4dd36701a24e25a', // Anwendungs-Geheimcode
+  'cookie' => true, // enable optional cookie support
+));
+ 
+if ($session = $facebook->getUser()) 
+{// Session vorhanden?
+  try 
+  {
+    $me = $facebook->api('/me'); // Daten üder API abfragen.
+    print '<pre>' . print_r($me, true) . '</pre>'; // Ausgabe der persönlichen Daten.
+    print '<pre>' . print_r($session, true) . '</pre>'; // Ausgabe der Sessiondaten.
+  } 
+  catch (FacebookApiException $e) 
+  {
+    print $e;
+  }
+} 
+else 
+{// Keine Session vorhanden.
+  $params = array(
+    'fbconnect' => 0,
+    'canvas' => 1,
+  );
+ 
+  $loginUrl = $facebook->getLoginUrl($params); // URL zum Autorisierungsdialog erstellen.
+  print '<script>top.location.href = "' . $loginUrl . '"</script>'; // Weiterleitung zum Anfordern der Autorisierung.
+}
 ?>
