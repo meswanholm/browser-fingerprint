4bee616bea0e6f033827600c46611b410d82fa05
tfriepes@gmail.com <tfriepes@gmail.com@34251659-b5cb-f043-1aff-89ad90e83b3e>
2012-05-07 21:43:45 +0000

    Felder rein

--- trunk/index2.php
+++ trunk/index2.php
@@ -7,7 +7,10 @@
     'appId' => '294040014004290',
     'secret' => '942bf9f89444df74c4dd36701a24e25a',
   );
-
+  
+  $permissions = "user_birthday, user_interests, user_photos, user_groups, user_likes, email, user_about_me, user_hometown, user_work_history";
+  $logout_url = "http://www.einfach-wir.net/daniel/fb/index2.php";
+  
   $facebook = new Facebook($config);
   $user_id = $facebook->getUser();
 ?>
@@ -22,7 +25,11 @@
       try 
 	  {
         //Benutzer Informationen, davor Erlaubnis erfragen
+		$access_token = $facebook->getAccessToken();
+		
+		//http://developers.facebook.com/docs/reference/api/user/
 		$user_profile = $facebook->api("/" . $user_id, "GET");
+		print '<pre>' . print_r($user_profile, true) . '</pre>'; 
 		//ID
 		echo "ID: " . $user_profile['id'];
 		echo "</br>";
@@ -30,50 +37,126 @@
 		echo "Name: " . $user_profile['name'];
 		echo "</br>";
 		//HOCHSCHULE/SCHULE
-		echo $user_profile['education']['0']['type'] . ": " . $user_profile['education']['0']['school']['name'];
-		echo "</br>";
+		if ($user_profile['education']['0']['school']['name'] != "")
+		{
+			echo $user_profile['education']['0']['type'] . ": " . $user_profile['education']['0']['school']['name'];
+			echo "</br>";
+		}
 		//GESCHLECHT
-		if ($user_profile['gender'] == 'male')
-			echo "Geschlecht: m&aumlnnlich"; 
-		else
-			echo "Geschlecht: weiblich";
+		if ($user_profile['gender'] != "")
+		{
+			if ($user_profile['gender'] == 'male')
+				echo "Geschlecht: m&aumlnnlich"; 
+			else
+				echo "Geschlecht: weiblich";
+			echo "</br>";
+		}
+		//GEBURTSTAG
+		if ($user_profile['birthday'] != "")
+		{
+			$birthday = $user_profile['birthday'];
+			echo "Geburtstag: " . $birthday[3] . $birthday[4] . "." . $birthday[0] . $birthday[1] . "." . $birthday[6] . $birthday[7] . $birthday[8] . $birthday[9];
+		}
 		//PROFILBILD
 		echo "<img src=\"http://graph.facebook.com/" . $user_id . "/picture?type=large\" />";
 		echo "</br>";
+		//E-MAIL
+		if ($user_profile['email'])
+		{
+			echo $user_profile['email'];
+			echo "</br>";
+		}
+		//STADT
+		if ($user_profile['hometown'] != "")
+		{
+			echo $user_profile['hometown'];
+			echo "</br>";
+		}
+		//ARBEIT
+		if ($user_profile != "")
+		{
+			echo $user_profile['work'];
+			echo "</br>";
+		}
+		
+		
+		
+		
+		
+		
+		
 		//FREUNDE
 		$friends = $facebook->api("/" . $user_id . "/friends", "GET"); 
 		$friendsData = $friends['data'];
+		$friendsSize = sizeof($friendsData);
 		//Anzahl Freunde
-		echo "Anzahl Freunde: " . sizeof($friendsData);
-		echo "</br>";
+		if ($friendsSize > 0)
+		{
+			echo "Anzahl Freunde: " . $friendsSize;
+			echo "</br>";
 		
-		for ($i = 0; $i < sizeof($friendsData); $i++) 
-		{ 
-           $friend = $friendsData[$i]; 
-                     echo $friend['name'];
-					 echo "</br>";
-        }; 
+			for ($i = 0; $i < $friendsSize; $i++) 
+			{ 
+				$friend = $friendsData[$i]; 
+                echo $friend['name'];
+				echo "</br>";
+			};
+		}
 		//GRUPPEN
 		$groups = $facebook->api("/" . $user_id . "/groups", "GET"); 
 		$groupsData = $groups['data'];
+		$groupsSize = sizeof($groupsData);
 		//Anzahl Gruppen
-		echo "Anzahl Gruppen: " . sizeof($groupsData);
-		echo "</br>";
+		if ($groupsSize > 0)
+		{
+			echo "Anzahl Gruppen: " . $groupsSize;
+			echo "</br>";
 		
-		for ($i = 0; $i < sizeof($groupsData); $i++) 
+			for ($i = 0; $i < $groupsSize; $i++) 
+			{ 
+				$group = $groupsData[$i]; 
+                echo $group['name'];
+				echo "</br>";
+			}; 
+		}
+		//SPRACHEN
+		$languages = $user_profile['languages'];
+		for ($i = 0; $i < sizeof($languages); $i++) 
 		{ 
-           $group = $groupsData[$i]; 
-                     echo $group['name'];
+           $language = $languages[$i]; 
+                     echo $language['name'];
 					 echo "</br>";
         }; 
-		//GEBURTSTAG
-		echo "Geburtstag: " . $user_profile['birthday'];
-		
-			
-			
-			
+		//SPIELE
+		$games = $facebook->api("/" .  $user_id . "/games", "GET");
+		$gamesData = $games['data'];
+		$gamesSize = sizeof($gamesData);
+		if ($gamesSize > 0)
+		{
+			for ($i = 0; $i < $gamesSize; $i++) 
+			{ 
+				$games = $games[$i]; 
+				echo $games['name'];
+				echo "</br>";
+			}; 
+		}
+		//LIKES
+		$likes = $facebook->api("/" .  $user_id . "/likes", "GET");
+		$likesData = $likes['data'];
+		$likesSize = sizeof($likesData);
+		if ($likesSize > 0)
+		{
+			for ($i = 0; $i < $likesSize; $i++) 
+			{ 
+				$likes = $likes[$i]; 
+				echo $likes['name'];
+				echo "</br>";
+			}; 
+		}
+	
+	
 		//Logout-Button Generierung
-		$params = array( 'next' => 'http://localhost/index2.php'); //Wo geht es hin nach dem Logout?
+		$params = array( 'next' => $logout_url); //Wo geht es hin nach dem Logout?
 		$logout_url = $facebook->getLogoutUrl($params); // $params is optional. 
 		echo '<p><a href="' . $logout_url . '">Logout</a></p>';
       } 
@@ -87,11 +170,11 @@
 		$params = array(
 			'fbconnect' => 0,
 			'canvas' => 1,
-			'req_perms' => 'user_birthday, user_interests, user_photos, user_groups',
+			'scope' => $permissions,
 		);
 
 		// No user, print a link for the user to login
-		$login_url = $facebook->getLoginUrl();
+		$login_url = $facebook->getLoginUrl($params);
         echo '<a href="' . $login_url . '">Login</a>';
 		echo "ERROR";
 		//print '<script>top.location.href = "' . $login_url . '"</script>'; // Weiterleitung zum Anfordern der Autorisierung
@@ -105,11 +188,11 @@
 	  $params = array(
 		'fbconnect' => 0,
 		'canvas' => 1,
-		'req_perms' => 'user_birthday, user_interests, user_photos, user_groups',
+		'scope' => $permissions,
 	  );
 
       // No user, print a link for the user to login
-      $login_url = $facebook->getLoginUrl();
+      $login_url = $facebook->getLoginUrl($params);
 	  //echo '<a href="' . $login_url . '">Login</a>';
 	  print '<script>top.location.href = "' . $login_url . '"</script>'; // Weiterleitung zum Anfordern der Autorisierung
     }
